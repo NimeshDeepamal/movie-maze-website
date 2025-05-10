26d4a8061337f31df6ffc1038b96bd697daf44e5
NimeshDeepamal <nimeshdeepamal@gmail.com>
2025-05-10 23:25:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ResponsiveAppBar from './Components/Navbar/ResponsiveAppBar';
-import Home from './Pages/Home'
+import Home from './Pages/Home';
+import MovieList from './Components/MovieList/MovieList';
+import Favorites from './Pages/Favorite';
 
 function App() {
   return (
-    <div className="App">
-      <ResponsiveAppBar />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <ResponsiveAppBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<MovieList type="popular" title="Popular Movies" />} />
+          <Route path="/top-rated" element={<MovieList type="top_rated" title="Top Rated Movies" />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
