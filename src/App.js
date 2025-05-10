ffbae2dad2bf0f5706f56a91811a188a2cda3644
NimeshDeepamal <nimeshdeepamal@gmail.com>
2025-05-10 12:32:33 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ResponsiveAppBar from './Components/Navbar/ResponsiveAppBar';
+import Home from './Pages/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <ResponsiveAppBar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
